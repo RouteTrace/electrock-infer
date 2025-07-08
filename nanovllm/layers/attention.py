e779f56d174e62e7b74dfadddc553ca0c71ad5d2
zhushengguang <1092841848@qq.com>
2025-07-08 14:29:50 +0800

    Add a interface for old flash_attn, not implement

--- nanovllm/layers/attention.py
+++ nanovllm/layers/attention.py
@@ -23,15 +23,16 @@ def store_kvcache_kernel(
     value_offsets = idx * value_stride + tl.arange(0, D)
     key = tl.load(key_ptr + key_offsets)
     value = tl.load(value_ptr + value_offsets)
-    slot = tl.load(slot_mapping_ptr + idx)
+    slot = tl.load(slot_mapping_ptr + idx) # slot 代表kv cache中每个token的槽位 -->k/v cache = (num_slot, num_kv_head, head_dim)
     cache_offsets = slot * D + tl.arange(0, D)
     tl.store(k_cache_ptr + cache_offsets, key)
     tl.store(v_cache_ptr + cache_offsets, value)
 
 
 def store_kvcache(key: torch.Tensor, value: torch.Tensor, k_cache: torch.Tensor, v_cache: torch.Tensor, slot_mapping: torch.Tensor):
-    N, num_heads, head_dim = key.shape
-    D = num_heads * head_dim
+    N, num_kvheads, head_dim = key.shape
+    D = num_kvheads * head_dim
+    # print(f"{key.shape=} , D = {num_heads*head_dim}")
     assert key.stride(-1) == 1 and value.stride(-1) == 1
     assert key.stride(1) == head_dim and value.stride(1) == head_dim
     assert k_cache.stride(1) == D and v_cache.stride(1) == D
@@ -56,12 +57,16 @@ class Attention(nn.Module):
         self.k_cache = self.v_cache = torch.tensor([])
 
     def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+        context = get_context() # 获取forward的相关信息
+        if context.enable_block_table:
+            # 兼容旧版本flash_attn
+            return self._forward_old_flash_attn(q, k, v, context)
+
         o: torch.Tensor
         q = q.view(-1, self.num_heads, self.head_dim)
         k = k.view(-1, self.num_kv_heads, self.head_dim)
         v = v.view(-1, self.num_kv_heads, self.head_dim)
-        context = get_context()
-        k_cache = self.k_cache
+        k_cache = self.k_cache # (total_block_num, block_size, num_kv_heads, head_dim)
         v_cache = self.v_cache
         store_kvcache(k, v, k_cache, v_cache, context.slot_mapping)
         if context.is_prefill:
@@ -78,4 +83,31 @@ class Attention(nn.Module):
         o = o.view(-1, self.num_heads * self.head_dim)
         return o
     
+    def _forward_old_flash_attn(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, ctx):
 
+        o: torch.Tensor
+        q = q.view(-1, self.num_heads, self.head_dim)
+        k = k.view(-1, self.num_kv_heads, self.head_dim)
+        v = v.view(-1, self.num_kv_heads, self.head_dim)
+        k_cache = self.k_cache 
+        v_cache = self.v_cache
+        store_kvcache(k, v, k_cache, v_cache, ctx.slot_mapping)
+        if ctx.is_prefill:
+            if ctx.block_tables is not None:    # prefix cache
+                k, v = k_cache, v_cache
+            o = flash_attn_varlen_func(q, k, v,
+                                       max_seqlen_q=ctx.max_seqlen_q, cu_seqlens_q=ctx.cu_seqlens_q,
+                                       max_seqlen_k=ctx.max_seqlen_k, cu_seqlens_k=ctx.cu_seqlens_k,
+                                       softmax_scale=self.scale, causal=True)
+        else:    # decode
+            # 要根据block_table去构造正确的kv
+            o = flash_attn_varlen_func(q.unsqueeze(1), k_cache, v_cache,
+                                        max_seqlen_k=1,max_seqlen_q=1,
+                                        cu_seqlens_k=ctx.cu_seqlens_k, cu_seqlens_q=ctx.cu_seqlens_q,
+                                        softmax_scale=self.scale, causal=True)
+        o = o.view(-1, self.num_heads * self.head_dim)
+        return o
+
+
+def _prepare_attention_kv(k_cache:torch.Tensor, v_cache:torch.Tensor, ):
+    pass
